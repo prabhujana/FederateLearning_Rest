35a722d5e89c140904f3c2a35b7b03775423760b
prabhujana <25284504+prabhujana@users.noreply.github.com>
2020-01-03 17:52:31 -0500

    Update train_async.py

--- mainserver/train_async.py
+++ mainserver/train_async.py
@@ -1,54 +1,71 @@
-import concurrent.futures
-import urllib.request
-# from urllib import HTTPError
-from fl_agg import model_aggregation
-from main_server import send_agg_to_client
+import ast
+import math
+import time
 import requests, json
+import concurrent.futures
+import asyncio, aiohttp
+
+from fl_agg import model_aggregation
+from main_server import send_agg_to_clients
+from requests.exceptions import HTTPError
 from datetime import datetime
-# URLS = ['http://www.foxnews.com/',
-#         'http://www.cnn.com/',
-#         'http://europe.wsj.com/',
-#         'http://www.bbc.co.uk/',
-#         'http://some-made-up-domain.com/']
 
-URLS = ['http://localhost:8001/modeltrain', 'http://localhost:8002/modeltrain']
-clients = ['http://localhost:8001/', 'http://localhost:8002/']
+# define rest end points here.
+workers = ['http://localhost:8001', 'http://localhost:8002']
 
-def fetch(url):
+# Performs model training by calling /modeltrain endpoint at the workers
+async def fetch(session, url):
     try:
-        url += 'modeltrain'
-        response = requests.get(url)
-        # print(datetime.now().strftime('%H:%M:%S'))
-        # print(url)
-        return response.text()
+        async with session.get(url + '/modeltrain') as response:
+            return await response.text()
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')  # Python 3.6
     except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
+        print('Other error occurred:', err)  # Python 3.6
     else:
         print('Success!')
 
-# Retrieve a single page and report the URL and contents
-def load_url(url, timeout):
-    with urllib.request.urlopen(url, timeout=timeout) as conn:
-        return conn.read()
+# Loop executes the core functions. epoch keeps track of global iter and current_worker is a list used after 1st epoch,
+# to keep track of which workers have completed tasks and ready for next round of training
+async def sync(epoch, current_worker):
+    g_tasks = []
+    task_res =[]
 
-# We can use a with statement to ensure threads are cleaned up promptly
-with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-    # Start the load operations and mark each future with its URL
-    print("start training")
-    print(datetime.now().strftime('%H:%M:%S'))
-    future_to_url = {executor.submit(fetch, url): url for url in clients}
-    for future in concurrent.futures.as_completed(future_to_url):
-        url = future_to_url[future]
-        try:
-            print("Done training")
-            print(url)
-            print(datetime.now().strftime('%H:%M:%S'))
-            model_aggregation()
-            send_agg_to_client(url)
+    # For 1st epoch we wait untill all worker finishses their training, we keep track of epochs
+    print('new_epoch:', epoch)
+    if epoch == 1:
+        print('current_worker:', current_worker)
+        async with aiohttp.ClientSession() as session:
+            for init_worker in workers:
+                g_tasks.append(fetch(session, init_worker))
+            done,pending = await asyncio.wait(g_tasks, return_when=asyncio.ALL_COMPLETED)
 
-        except Exception as exc:
-            print('%r generated an exception: %s' % (url, exc))
-        else:
-            print('%r page is %d bytes' % (url, len(data)))
+    else:
+        print('current_worker:', current_worker)
+        async with aiohttp.ClientSession() as session:
+            for worker in current_worker:
+                g_tasks.append(fetch(session, worker))
+                # Once new training is launched, remove the workerid from current_worker list to avoid repeated training
+                current_worker.remove(worker)
+            done,pending = await asyncio.wait(g_tasks,return_when=asyncio.FIRST_COMPLETED)
+
+    # For workers that finished tasks, perform aggregation and send the new model. Add the workerid to current_worker
+    # list for initiating next round of training
+    for i in done:
+        workerid = i.result()
+        model_aggregation()
+        send_agg_to_clients(workerid)
+        current_worker.append(workerid)
+        print('Done worker:', current_worker)
+        epoch = epoch + 1
+
+async def main_sync():
+    for i in range(1,50):
+        epoch = i
+        await sync(epoch, current_worker)
+
+epoch = 0
+current_worker = []
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main_sync())
+loop.run_forever()
