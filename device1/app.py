67ac4e11d4fc06c10a33b4c5699ab424db8c5be0
prabhujana <25284504+prabhujana@users.noreply.github.com>
2020-01-03 17:48:56 -0500

    Update app.py

--- device1/app.py
+++ device1/app.py
@@ -5,14 +5,14 @@ import requests
 import ast
 from model_train import train
 import time
-import argparse
-import sys
 
 app = Flask(__name__)
 
+device_id = "http://localhost:8001"
+
 @app.route('/')
 def hello():
-	return "Device 1"
+	return device_id
 
 @app.route('/sendstatus', methods=['GET'])
 def send_status():
@@ -25,7 +25,7 @@ def send_status():
 	print(r, r.status_code, r.reason, r.text)
 	if r.status_code == 200:
 		print("yeah")
-	
+
 	return "Status OK sent !"
 
 @app.route('/sendmodel')
@@ -37,10 +37,8 @@ def send_model():
 		'model': ('model1.npy', file, 'application/octet-stream')
 	}
 
-	req = requests.post(url='http://localhost:8000/cmodel', 
-						files=files)
-	# print(req.text)
-	return "Model sent !"
+	req = requests.post(url='http://localhost:8000/cmodel',files=files)
+	print("Model sent !")
 
 @app.route('/aggmodel', methods=['POST'])
 def get_agg_model():
@@ -54,67 +52,20 @@ def get_agg_model():
 
 		wfile = open("model_update/"+fname, 'wb')
 		wfile.write(file)
-			
+
 		return "Model received!"
+
 	else:
 		return "No file received!"
 
 @app.route('/modeltrain')
 def model_train():
 	print("Received request")
-
-	# time.sleep(10)
 	train()
 	print("Model trained successfully!")
 	send_status()
 	send_model()
-	# get_agg_model()
-	return "Model trained successfully!"
-
-
-def define_and_get_arguments(args=sys.argv[1:]):
-    # Parse args
-    parser = argparse.ArgumentParser(description="Run websocket server worker.")
-    parser.add_argument(
-        "--port",
-        "-p",
-        type=int,
-		default=8001,
-        help="port number of the websocket server worker, e.g. --port 8777",
-    )
-    parser.add_argument("--host", type=str, default="localhost", help="host for the connection")
-    parser.add_argument(
-        "--id", type=str, help="name (id) of the websocket server worker, e.g. --id alice"
-    )
-
-
-    args = parser.parse_args(args=args)
-    return args
+	return device_id
 
 if __name__ == '__main__':
-	
-	args = define_and_get_arguments(sys.argv[1:])
-	id=args.id
-	host=args.host
-	port=args.port
-	print(port)
-	app.run(host=host, port=int(port), debug=False, use_reloader=True)
-	# app.run(host='localhost', port=8002, debug=False, use_reloader=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	app.run(host='localhost', port=8001, debug=False, use_reloader=True)
