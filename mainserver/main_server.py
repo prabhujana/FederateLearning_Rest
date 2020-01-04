8d5871d721b27c080a4c016a865a022005752bc6
prabhujana <25284504+prabhujana@users.noreply.github.com>
2020-01-04 17:51:39 -0500

    Update main_server.py

--- mainserver/main_server.py
+++ mainserver/main_server.py
@@ -5,72 +5,45 @@ from fl_agg import model_aggregation
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "Server running !"
+workers = []
 
-@app.route('/clientstatus', methods=['GET','POST'])
-def client_status():
-	url = "http://localhost:8001/serverack"
+def reg_workers():
+	return workers
 
-	if request.method == 'POST':
-		client_port = request.json['client_id']
-
-		with open('clients.txt', 'a+') as f:
-			f.write('http://localhost:' + client_port + '/\n')
-
-		print(client_port)
-
-		if client_port:
-			serverack = {'server_ack': '1'}
-			# response = requests.post( url, data=json.dumps(serverack), headers={'Content-Type': 'application/json'} )
-			return str(serverack)
-		else:
-			return "Client status not OK!"
-	else:
-		return "Client GET request received!"
+@app.route('/register', methods=['POST'])
+def register_worker():
+		if request.method == 'POST':
+			worker_id = request.data.decode('UTF-8')
+			if worker_id not in workers:
+				workers.append(request.data.decode('UTF-8'))
+				print(workers)
+			return "Worker Registered"
 
 @app.route('/secagg_model', methods=['POST'])
 def get_secagg_model():
 	if request.method == 'POST':
 		file = request.files['model'].read()
 		fname = request.files['json'].read()
-		# cli = request.files['id'].read()
-
 		fname = ast.literal_eval(fname.decode("utf-8"))
 		cli = fname['id']+'\n'
 		fname = fname['fname']
-
-		# with open('clients.txt', 'a+') as f:
-		# 	f.write(cli)
-
-		# print(fname, cli)
 		wfile = open("agg_model/"+fname, 'wb')
 		wfile.write(file)
-
 		return "Model received!"
 	else:
 		return "No file received!"
-
 
 @app.route('/cmodel', methods=['POST'])
 def getmodel():
 	if request.method == 'POST':
 		file = request.files['model'].read()
 		fname = request.files['json'].read()
-		# cli = request.files['id'].read()
-
 		fname = ast.literal_eval(fname.decode("utf-8"))
 		cli = fname['id']+'\n'
 		fname = fname['fname']
-
-		# with open('clients.txt', 'a+') as f:
-		# 	f.write(cli)
-
 		print(fname)
 		wfile = open("client_models/"+fname, 'wb')
 		wfile.write(file)
-
 		return "Model received!"
 	else:
 		return "No file received!"
@@ -80,12 +53,10 @@ def perform_model_aggregation():
 	model_aggregation()
 	return 'Model aggregation done!\nGlobal model written to persistent storage.'
 
-
 @app.route('/send_model_clients')
 def send_agg_to_clients(i):
 	clients = []
 	clients.append(i)
-
 	print('Clients:',clients)
 
 	for c in clients:
@@ -103,7 +74,6 @@ def send_agg_to_clients(i):
 
 @app.route('/send_agg_to_client')
 def send_agg_to_client(client):
-
 	if client != '':
 		file = open("agg_model/agg_model.h5", 'rb')
 		data = {'fname':'agg_model.h5'}
@@ -111,14 +81,10 @@ def send_agg_to_client(client):
 			'json': ('json_data', json.dumps(data), 'application/json'),
 			'model': ('agg_model.h5', file, 'application/octet-stream')
 		}
-
 		print(c+'aggmodel')
 		req = requests.post(url=c+'/aggmodel', files=files)
 		print(req.status_code)
-
-	# print(req.text)
 	return "Aggregated model sent !"
-
 
 if __name__ == '__main__':
 	app.run(host='localhost', port=8000, debug=False, use_reloader=True)
